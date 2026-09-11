33f82c8c84ac5ae64b810e428310e2fb962c9238
Mxthxzy000 <quirinojulio77@gmail.com>
2026-09-11 11:07:49 -0300

    página home arrumada

--- Modulo_A/HELPDESK_INDUSTRIAL/pages/homePage.php
+++ Modulo_A/HELPDESK_INDUSTRIAL/pages/homePage.php
@@ -13,41 +13,15 @@ $resultado = $conexao->query($sql);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Helpdesk Industrial</title>
+    <link rel="stylesheet" href="../styles/styleGeral.css">
 </head>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 0;
-            background-color: #f4f4f4;
-        }
-
-        header {
-            background-color: #686868;
-            color: white;
-            padding: 20px;
-            text-align: center;
-        }
-
-        button {
-            background-color: #93CC64;
-            color: white;
-            border: none;
-            padding: 10px 20px;
-            cursor: pointer;
-            font-size: 16px;
-        }
-
-        button:hover {
-            background-color: #218838;
-        }
-    </style>
 <body>
     <header>
         <h1>Bem-vindo ao Helpdesk Industrial</h1>
-        <button onclick="window.location.href='formulario.php'">Formulário</button>
+        <button onclick="window.location.href='formulario.php'" class="btnnav" >Formulário</button>
     </header>
 
+<<<<<<< Updated upstream
     <h1>Chamados Realizados</h1>
 
     <?php while ($chamado = $resultado->fetch_assoc()): ?>
@@ -72,5 +46,7 @@ $resultado = $conexao->query($sql);
 
     <?php endwhile; ?>
 
+=======
+>>>>>>> Stashed changes
 </body>
 </html>
