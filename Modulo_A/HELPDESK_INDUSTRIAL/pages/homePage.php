d566bc43e407c0fb16592a59488dec1828f1140a
Mxthxzy000 <quirinojulio77@gmail.com>
2026-09-11 11:10:08 -0300

    página home arrumada

--- Modulo_A/HELPDESK_INDUSTRIAL/pages/homePage.php
+++ Modulo_A/HELPDESK_INDUSTRIAL/pages/homePage.php
@@ -20,33 +20,5 @@ $resultado = $conexao->query($sql);
         <h1>Bem-vindo ao Helpdesk Industrial</h1>
         <button onclick="window.location.href='formulario.php'" class="btnnav" >Formulário</button>
     </header>
-
-<<<<<<< Updated upstream
-    <h1>Chamados Realizados</h1>
-
-    <?php while ($chamado = $resultado->fetch_assoc()): ?>
-
-        <div>
-
-            <h2>
-                <?= htmlspecialchars($chamado["titulo"]) ?>
-            </h2>
-
-            <p>
-                Solicitante:
-                <?= htmlspecialchars($chamado["solicitante"]) ?>
-            </p>
-
-            <p>
-                Setor:
-                <?= htmlspecialchars($chamado["setor"]) ?>
-            </p>
-
-        </div>
-
-    <?php endwhile; ?>
-
-=======
->>>>>>> Stashed changes
 </body>
 </html>
